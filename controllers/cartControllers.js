52654cf216938c135ed2187988e50c76334e5a9e
unknown <tano@quierostandup.com>
2021-04-13 21:49:44 -0300

    order and cart Controller added to the project

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -19,3 +19,73 @@ module.exports.get_cart_items = async (req, res) => {
 
 }
 
+module.exports.add_cart_item = async (req, res) => {
+    const userId = req.params.id;
+    const {productId, quantity} = req.body;
+
+    try {
+        let cart = await Cart.findOne({userId});
+        let item = await Item.findOne({_id: productId});
+        if (!item) {
+            res.status(404).send('item not found');
+        }
+        const price = item.price;
+        const name = item.title;    
+        
+        if(cart) {
+            //if the cart exist for this particular user
+            let itemIndex = cart.items.findIndex(p => p.productId == productId);
+
+            //check if products exist or not 
+            if(itemIndex > -1)
+            {
+                let productItem = cart.items[itemIndex];
+                productItem.quantity += quantity;
+                cart.items[itemIndex] = productItem;
+            }
+            else {
+                cart.items.push({ productId, name, quantity, price, });
+            }
+            cart.bill += quantity*price;
+            cart = await cart.save();
+            return res.status(201).send(cart);
+        }
+        else {
+            // No cart exist , create one
+            const newCart = await Cart.create({
+                userId, 
+                items: [{productId, price, quantity, name}],
+                bill: quantity*price,
+
+            });
+            return res.status(201).send(newCart);
+        }
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).send('Something went wrong! ')
+    }
+
+}
+
+module.exports.delete_item = async (req, res) => {
+    const userId = req.params.id;
+    const productId = req.params.itemId;
+    
+    try {
+        let cart = await Cart.findOne({userId});
+        let itemIndex = cart.items.findIndex(p => p.productId == productId);
+        if(itemIndex > -1) {
+            let productItem = cart.items[itemIndex];
+            cart.bill -= productItem.quantity * productItem.price;
+            cart.items.splice(itemIndex,1);
+        }
+        cart = await cart.save();
+        return res.status(201).send(cart);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).send('Something went wrong');
+    }
+}
+
